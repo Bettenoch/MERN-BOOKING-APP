d7a8da61cbe94d567b88948c3f673a6ef6ac25ad
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-02-14 10:10:56 +0300

    show toast message on success and error.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form"
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client';
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string;
@@ -11,14 +13,16 @@ export type RegisterFormData = {
 }
 
 const Register = () => {
-
+    const {showToast} = useAppContext()
+    const navigate = useNavigate();
     const { register, watch, handleSubmit, formState:{ errors}} = useForm <RegisterFormData>();
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
-            console.log('register')
+           showToast({message: 'Registartion sucessful', type: 'SUCCESS'});
+           navigate('/');
         },
         onError: (error: Error) => {
-            console.log(error.message)
+            showToast({ message: error.message, type: 'ERROR'});
         }
     });
 
