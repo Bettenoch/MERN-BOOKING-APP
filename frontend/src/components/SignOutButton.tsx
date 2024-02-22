ec0e8fd6a9cbfe0ad8ae81d8956e750d85e94c5b
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-02-22 20:31:27 +0300

    create sign  out button component

---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,11 @@
+
+const SignOutButton = () => {
+  return (
+    <button className=' text-blue-600 px-3 font-bold hover:bg-gray-100 bg-white'>
+    Sign Out
+     </button>
+  )
+
+}
+
+export default SignOutButton
